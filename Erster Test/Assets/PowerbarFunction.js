efbd0d69b44d722daebb78a79647fbcd5432c912
Benjamin Bauer <benjamin.bauer@stud.h-da.de>
2014-11-02 23:14:56 +0100

    Powerbar hinzugefügt

---
+++ Erster Test/Assets/PowerbarFunction.js
@@ -0,0 +1,32 @@
+﻿#pragma strict
+private var powerbarTrans : Transform;
+private var antHPfkt : antHP;
+private var antTrans : Transform;
+public var offset : Vector2;
+
+function Awake(){
+
+var antHPobj = GameObject.Find("ant1");
+antHPfkt = antHPobj.GetComponent(antHP);
+antTrans = antHPobj.GetComponent(Transform);
+powerbarTrans = GetComponent(Transform);
+
+}
+
+function Start () {
+
+
+
+}
+
+function Update () {
+
+powerbarTrans.localScale.x=(antHPfkt.HPvalueAnt1/500);
+powerbarTrans.position = antTrans.position + offset;
+
+	if(antHPfkt.HPvalueAnt1<=0){
+		gameObject.active=false;
+	}
+
+
+}
