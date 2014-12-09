64be83d0b9bd577e6f98310e8fd308a684ce7ff6
Benjamin Bauer <benjamin.bauer@stud.h-da.de>
2014-12-09 08:37:59 +0100

    Buttons mit Problemen

--- Erster Test/Assets/Ant/Scripts/NewBulletShoot.js
+++ Erster Test/Assets/Ant/Scripts/NewBulletShoot.js
@@ -11,16 +11,22 @@ public  var ShootIsPressed : boolean = false; //Für GameSystem
 
 private var GameSystemObj : GameObject;
 private var GameSystemFkt : GameSystem;
+
+public var buttonShootImage : Texture2D;
+public var buttonSize : int = 70;
+
+private var RotateButtonFkt : RotateButtons;
      
 function Start () {
 	ShootingPointFunction=GetComponentInChildren(ShootPointFkt);
 	GameSystemObj=GameObject.Find("GameSystem");
 	GameSystemFkt=GameSystemObj.GetComponent(GameSystem);
+	RotateButtonFkt=GetComponentInParent(RotateButtons);
 	
 }
   
 function Update () {
-     if (Input.GetMouseButton(0)&& shootingPower<MaxShootingPower &&alreadyShoot==false && GameSystemFkt.bulletIsFlying==false) {
+    /* if (Input.GetMouseButton(0)&& shootingPower<MaxShootingPower &&alreadyShoot==false && GameSystemFkt.bulletIsFlying==false) {
      	shootingPower=shootingPower+shootingPowerSpeed;//Lädt Power auf
      }
      if (Input.GetMouseButtonUp(0)&&alreadyShoot==false && GameSystemFkt.bulletIsFlying==false) {
@@ -33,10 +39,38 @@ function Update () {
          go.rigidbody2D.AddForce(go.transform.up * shootingPower);
          alreadyShoot=true;
          ShootIsPressed=true;//wird von GameSystem zurück gestellt
-     }
-     if(GameSystemFkt.bulletIsFlying==false && alreadyShoot==true){
+     }*/
+    if(GameSystemFkt.bulletIsFlying==false && alreadyShoot==true){
      	shootingPower=0;
      	alreadyShoot=false;
      }
+     if(gameObject.active==true){
+     	RotateButtonFkt.enabled=true;
+     }
+     else{
+     	RotateButtonFkt.enabled=false;
+     }
+     //Funktion um Drehknöpfe zu aktivivieren
  }
+ 
+ function OnGUI () {
+
+	if (GUI.Button(Rect(15,225,buttonSize,buttonSize),buttonShootImage)){//&&alreadyShoot==false && GameSystemFkt.bulletIsFlying==false) {
+         var pos = ShootingPointFunction.ShootPointPosition;
+         //pos.z = transform.position.z - Camera.main.transform.position.z;
+         //pos = Camera.main.ScreenToWorldPoint(pos);
+
+         var q = Quaternion.FromToRotation(Vector3.up, pos - transform.position);
+         var go = Instantiate(prefab, transform.position, q);
+         go.rigidbody2D.AddForce(go.transform.up * shootingPower);
+         alreadyShoot=true;
+         ShootIsPressed=true;//wird von GameSystem zurück gestellt
+     }
+     //Wenn ich Button gedrückt halte
+	 while (GUI.RepeatButton(Rect(15,155,buttonSize,buttonSize),buttonShootImage)){
+		if (shootingPower<MaxShootingPower &&alreadyShoot==false && GameSystemFkt.bulletIsFlying==false) {
+     		shootingPower=shootingPower+shootingPowerSpeed;//Lädt Power auf
+     	}
+	}	
+}
 
---
+++ Erster Test/Assets/RotateButtons.js
@@ -0,0 +1,30 @@
+﻿#pragma strict
+
+public var buttonTurnImageUp : Texture2D;
+public var buttonTurnImageDown : Texture2D;
+public var buttonSize : int = 70;
+public var ButtonSpeed : float = 0.01; //Geschwindigkeit in dem gedreht wird
+public var maxAngleUp : float = 90;
+public var maxAngleDown : float = 300;
+
+function Awake () {
+
+}
+
+function OnGUI () {
+
+	//Wenn ich hoch-Button klicke
+	while (GUI.RepeatButton(Rect(15,15,buttonSize,buttonSize),buttonTurnImageUp)){
+		if(transform.rotation.z>maxAngleDown||transform.rotation.z<=maxAngleUp){
+			transform.rotation.z+=ButtonSpeed;
+		}
+	}
+	//Wenn ich runter-Button klicke
+	while (GUI.RepeatButton(Rect(15,85,buttonSize,buttonSize),buttonTurnImageDown)){
+		if(transform.rotation.z>maxAngleDown||transform.rotation.z<=maxAngleUp){
+			transform.rotation.z-=ButtonSpeed;
+		}
+	}
+	
+
+}
