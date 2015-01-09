1bddb9abada27ee9976b19a7df7ccd0cfe69fbda
Benjamin Bauer <benjamin.bauer@stud.h-da.de>
2015-01-09 18:01:37 +0100

    Ladebalken für Power
    Ist in Form eines Kreises

---
+++ Erster Test/Assets/PowerBarRadius.js
@@ -0,0 +1,15 @@
+﻿#pragma strict
+
+private var bulletFkt : NewBulletShoot;
+private var Trans : Transform;
+private var FullSize : float = 0.6;
+
+function Start () {
+	bulletFkt=GetComponentInParent(NewBulletShoot);
+	Trans = GetComponent(Transform);
+}
+
+function Update () {
+	Trans.localScale.x=0.4+FullSize*(bulletFkt.shootingPower/bulletFkt.MaxShootingPower);
+	Trans.localScale.y=0.4+FullSize*(bulletFkt.shootingPower/bulletFkt.MaxShootingPower);
+}
---
+++ Erster Test/Assets/ShootBarHide.js
@@ -0,0 +1,18 @@
+﻿#pragma strict
+
+private var bulletFkt : NewBulletShoot;
+private var GameSystemObj : GameObject;
+private var GameSystemFkt : GameSystem;
+
+function Start () {
+	bulletFkt=GetComponentInParent(NewBulletShoot);
+	GameSystemObj=GameObject.Find("GameSystem");
+	GameSystemFkt=GameSystemObj.GetComponent(GameSystem);
+}
+
+function Update () {
+	var percent = (bulletFkt.shootingPower/bulletFkt.MaxShootingPower);
+	if(percent<=0.01 || GameSystemFkt.bulletIsFlying==true){//gibt eine Fehlertoleranz
+		gameObject.active=false;
+	}
+}
