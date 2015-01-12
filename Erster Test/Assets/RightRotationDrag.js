875aebd9049b44d578979b77a5ef8e9daf99d356
Benjamin Bauer <benjamin.bauer@stud.h-da.de>
2015-01-12 15:12:56 +0100

    Winkel wird nach Schuss zurück gesetzt

--- Erster Test/Assets/RightRotationDrag.js
+++ Erster Test/Assets/RightRotationDrag.js
@@ -4,9 +4,27 @@
  private var v : float;
  private var horozontalSpeed : float = 2.0;
  private var verticalSpeed : float = 2.0;
+ public var PlayerIsMoving : boolean = false;
+ 
+ public var StandardRotationPos : float = 0;
+ public var RotationSpeed = 45.0;
+ 
+ private var GameSystemObj : GameObject;
+ private var GameSystemFkt : GameSystem;
+ 
+ function Start () {
+	GameSystemObj=GameObject.Find("GameSystem");
+	GameSystemFkt=GameSystemObj.GetComponent(GameSystem);
+ }
  
  function Update()
  {
+ 	if(PlayerIsMoving==false){
+	 	MoveToNormalRotation();
+	 }
+	 if(GameSystemFkt.bulletIsLanded==true){
+	 	PlayerIsMoving=false;
+	 }
  }
  
  function RoationByDraging(){
@@ -22,5 +40,12 @@
              v = verticalSpeed * touch.deltaPosition.y ;
              transform.Rotate( 0, 0, -v, Space.World );
          }
-     }
+     PlayerIsMoving=true;//Damit Ameise sich während des justierens nicht zurück dreht
+     	 
+ 	}
+ }
+ 
+ function MoveToNormalRotation(){
+		var angle : float = Mathf.MoveTowardsAngle(transform.eulerAngles.z, StandardRotationPos, RotationSpeed * Time.deltaTime);
+		transform.eulerAngles = Vector3(0, 0, angle);	
  }
